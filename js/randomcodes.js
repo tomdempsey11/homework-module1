90b700d7f8c05dc2c59a5023a0a3325ac29d38fa
Tom Dempsey <tomdempsey@Mac.lan>
2025-02-03 16:48:44 -0800

    created JS pages and created randomcode on contact page

---
+++ js/randomcodes.js
@@ -0,0 +1,27 @@
+/* ------ RANDOM CODES ------ */
+
+//Function to generate combination of characters
+function generateCode() {
+    //create variables to store generated codes and the type of characters we want to show as codes
+    var code = ' '; //initialize to null value
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$'
+
+    //generate character multiple times using a loop
+    for (i = 1; i <= 8; i++) {
+        var char = Math.random()* str.length; //random select a character from the variable
+        code += str.charAt(char) //accumulate the generated character into a string of 8
+
+    }
+    return code; //return the final accumulated string when loop ends
+}
+
+//Get HTML element to display
+document.getElementById("codes").innerHTML = generateCode();
+
+//disable button
+function disableButton() {
+    document.getElementById("submit").disabled = true;
+}
+
+//activate function
+disableButton();
